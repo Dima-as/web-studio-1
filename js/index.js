b38ae80a8cbb923e4d40da43c62070a5bbe9ea3d
Dima-as <80468495+Dima-as@users.noreply.github.com>
2022-12-11 22:55:03 +0200

    fix

--- js/index.js
+++ js/index.js
@@ -1,24 +1,29 @@
 (() => {
    const refs = {
+		mobilOpen: document.querySelector('[data-mobil-open]'),
+		mobilClose:document.querySelector('[data-mobil-close]'),
+		mobil:document.querySelector('[data-mobil]'),
+
       openModalBtn: document.querySelector('[data-modal-open]'),
       closeModalBtn: document.querySelector('[data-modal-close]'),
       modal: document.querySelector('[data-modal]'),
 		body:document.querySelector('body')
+
 	
    };
-     console.log(refs.body);
    refs.openModalBtn.addEventListener('click', toggleModal);
    refs.closeModalBtn.addEventListener('click', toggleModal);
+	refs.mobilOpen.addEventListener('click',toggleMobil);
+	refs.mobilClose.addEventListener('click',toggleMobil);
+
 
    function toggleModal() {
       refs.modal.classList.toggle('is-hidden');
 		refs.body.classList.toggle('body-overflow')
    }
-})();
-
-//  let item =''
-// filter.forEach(element => {
-// 	item=element
+	function toggleMobil() {
+      refs.mobil.classList.toggle('is-hidden');
+		refs.body.classList.toggle('body-overflow')
+   }
 	
-// });
-// console.log(item);
+})();
