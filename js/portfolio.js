4e81a4f198a1fa4dfc56289f2324e7d34047c948
Dima-as <80468495+Dima-as@users.noreply.github.com>
2022-12-19 23:22:29 +0200

    fix

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,1 +1,18 @@
+(() => {
+   const refs = {
+		mobilOpen: document.querySelector('[data-mobil-open]'),
+		mobilClose:document.querySelector('[data-mobil-close]'),
+		mobil:document.querySelector('[data-mobil]'),
+		body:document.querySelector('body')
 
+	
+   };
+	refs.mobilOpen.addEventListener('click',toggleMobil);
+	refs.mobilClose.addEventListener('click',toggleMobil);
+
+	function toggleMobil() {
+      refs.mobil.classList.toggle('is-hidden');
+		refs.body.classList.toggle('body-overflow')
+   }
+	
+})();
