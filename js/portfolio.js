f9dbb3247ad8de37a00475a8d8dfc66ad33447e2
Dima-as <80468495+Dima-as@users.noreply.github.com>
2022-12-25 23:11:37 +0200

    add

--- js/portfolio.js
+++ js/portfolio.js
@@ -3,16 +3,25 @@
 		mobilOpen: document.querySelector('[data-mobil-open]'),
 		mobilClose:document.querySelector('[data-mobil-close]'),
 		mobil:document.querySelector('[data-mobil]'),
-		body:document.querySelector('body')
+		body:document.querySelector('body'),
+		cardsModalOpen:document.querySelector('[data-cards-open]'),
+		cardsModalClose:document.querySelector('[data-cards-close]'),
+		cardsModal:document.querySelector('[data-cards]')
 
 	
    };
 	refs.mobilOpen.addEventListener('click',toggleMobil);
 	refs.mobilClose.addEventListener('click',toggleMobil);
+	refs.cardsModalOpen.addEventListener('click',toggleCards);
+	refs.cardsModalClose.addEventListener('click',toggleCards)
 
 	function toggleMobil() {
       refs.mobil.classList.toggle('is-hidden');
 		refs.body.classList.toggle('body-overflow')
    }
+	function toggleCards(){
+		refs.cardsModal.classList.toggle('is-hidden')
+		refs.body.classList.toggle('body-overflow')
+	}
 	
 })();
